e010e936036fd368eac395f9cf6c92e3319a7272
George Mtz <jmartinez@mconsultors.com>
2013-11-28 21:56:47 -0600

    Basic functionality and tests

--- apex-connect.js
+++ apex-connect.js
@@ -1,33 +1,37 @@
+var request = require("request")
+
 /**
  * Connect object constructor
  * @constructor
  */
-var Connect = function(){
+var Connect = function Connect(server, secret){
   //constructor
+
+  /**
+   * Apex server hostname
+   * @type {null}
+   */
+  this.server = server
+
+  /**
+   * Secret used to connect to the Apex server
+   * @type {null}
+   */
+  this.secret = secret
+
+  /**
+  * Auth token received from Apex
+  * @type {null}
+  */
+  this.auth_token = null
+
+  /**
+   * User token received
+   * @type {null}
+   */
+  this.user_token = null
 }
 
-/**
- * Apex server hostname
- * @type {null}
- */
-Connect.prototype.server = null
-
-/**
- * Secret used to connect to the Apex server
- * @type {null}
- */
-Connect.prototype.secret = null
-
-/**
- * Auth token received from Apex
- * @type {null}
- */
-Connect.prototype.auth_token = null
-/**
- * User token received
- * @type {null}
- */
-Connect.prototype.user_token = null
 
 /**
  * Initial connect call that establishes the auth
@@ -35,16 +39,31 @@ Connect.prototype.user_token = null
  *
  * Fires fn on completion with fn(err,auth_token)
  *
- * @param server
- * @param secret
  * @param fn
  */
-Connect.prototype.connect = function(server,secret,fn){
-  Connect.server = server
-  Connect.secret = secret
-  //do some sort of connect request
-  Connect.auth_token = "received from server"
-  fn(null,Connect.auth_token)
+Connect.prototype.connect = function(fn){
+
+  var self = this
+  request.post( this.server + "/auth/connect",
+    {form:{ secret:this.secret }},
+    function(error, response, body){
+      if (response.statusCode == 201) {
+        auth_response = JSON.parse(body)
+        if (auth_response.auth && auth_response.auth == "ok") {
+          self.auth_token = auth_response.token
+          fn(null, auth_response.token)
+        }else{
+          self.auth_token = null
+          fn("Could not authenticate", null)
+        }
+      }else{
+        self.auth_token = null
+        fn("Could not contact server : " + response.statusCode , null)
+      }
+    }
+  )
+
+  //fn(null, this.auth_token)
 }
 
 /**
@@ -53,17 +72,41 @@ Connect.prototype.connect = function(server,secret,fn){
  * Fires fn on completion with fn(err,user_token)
  *
  * @param collection
- * @param auth_id
- * @param auth_password
+ * @param id
+ * @param secret
  * @param fn
  */
-Connect.prototype.authorize = function(collection,auth_id,auth_password,fn){
-  Connect.collection = collection
-  Connect.auth_id = auth_id
-  Connect.auth_password = auth_password
-  //do some sort of auth request
-  Connect.user_token = "received from server"
-  fn(null,Connect.user_token)
+Connect.prototype.authorize = function(collection, id, secret, fn){
+  var self = this
+
+  if (!self.auth_token) {
+    fn("Not connected to the server", null)
+    return
+  }
+
+  request.post( this.server + "/auth/authorize",
+    {
+      form:{
+        token : self.auth_token,
+        collection : collection,
+        id: id,
+        secret : secret
+      }
+    },
+    function(error, response, body){
+      if (response.statusCode == 201) {
+        auth_response = JSON.parse(body)
+        if (auth_response.auth && auth_response.auth == "ok") {
+          fn(null, auth_response.user_token)
+        }else{
+          fn("Could not authenticate", null)
+        }
+      }else{
+        self.auth_token = null
+        fn("Could not contact server : " + response.statusCode , null)
+      }
+    }
+  )
 }
 
 //export module
--- apex-connect.test.js
+++ apex-connect.test.js
@@ -1,8 +1,68 @@
-require("should")
+var should = require("should")
+var Connect = require("./apex-connect.js")
+var nock = require('nock')
 
-describe("Apex Connect",function(){
-  it("should have a test that doesnt fail",function(){
-    var one = 1
-    one.should.equal(1)
+
+var server = "http://localhost"
+var secret = "tehSecret"
+
+var nockServer = nock(server)
+  .post('/wrongpath/auth/connect', {"secret": "" }).reply(404)
+  .post('/auth/connect', {"secret": secret }).reply(201,{"auth": "ok", "token":"1234"})
+  .post('/auth/connect', {"secret": "" }).reply(201,{"auth": "fail", "token":""})
+  .post('/auth/connect', {"secret": secret }).reply(201,{"auth": "ok", "token":"1234"})
+  .post('/auth/authorize', {"token": "1234" , "id": "user" , "secret" : "", "collection" : "staff" }).reply(201,{"auth": "fail", "user_token":""})
+  .post('/auth/authorize', {"token": "1234" , "id": "user" , "secret" : "1234", "collection" : "staff" }).reply(201,{"auth": "ok", "user_token":"ABCD"})
+
+
+describe("Connect and authenticate",function(){
+  it("Should not connect to the apex server",function(t){
+
+    var c = new Connect(server + "/wrongpath", "")
+    c.connect(function(err, token){
+      should.not.exist(token)
+      should.exist(err)
+      err.should.equal("Could not contact server : 404")
+      t();
+    })
   })
+  it("Fail auth to the apex server",function(t){
+    var c = new Connect(server , "")
+    c.connect(function(err, token){
+      should.not.exist(token)
+      should.exist(err)
+      err.should.equal("Could not authenticate")
+      t();
+    })
+  })
+  it("Auth to the apex server",function(t){
+    var c = new Connect(server , secret)
+    c.connect(function(err, token){
+      should.exist(token)
+      should.not.exist(err)
+      t();
+    })
+  })
+})
+
+describe("Authorize users",function(){
+  var c = new Connect(server , secret)
+
+  it("Should connect",function(t){
+    c.connect(function(err, token){
+        should.exist(token)
+        should.not.exist(err)
+        t()
+    })
+  })
+
+  it("Should authorize user",function(t){
+    c.authorize("staff", "user", "1234",
+      function(err, user_token){
+        should.exist(user_token)
+        should.not.exist(err)
+        t()
+    })
+  })
+
 })
